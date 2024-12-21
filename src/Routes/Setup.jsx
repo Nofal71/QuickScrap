6213453c7e56da1d414a02be50c4f535a946c44a
Nofal <147974175+Nofal71@users.noreply.github.com>
2024-12-21 09:04:07 +0500

    first commit

---
+++ src/Routes/Setup.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import SignInPage from '../Authentications/LoginPage';
+import SignUpPage from '../Authentications/SignupPage';
+import NavBar from '../Components/page-components/NavBar';
+
+const MainLayout = ({ children }) => (
+  <>
+    <NavBar />
+    {children}
+  </>
+);
+
+const AuthLayout = ({ children }) => (
+  <>
+    {children}
+  </>
+);
+
+const Setup = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <AuthLayout>
+              <SignInPage />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path='/signup'
+          element={
+            <AuthLayout>
+              <SignUpPage />
+            </AuthLayout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Setup;
